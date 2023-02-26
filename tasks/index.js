7603ff736d2f152d1928488a300bffb4005e6db9
dexter <dexterdevmode@gmail.com>
2023-02-26 23:55:16 +0530

    Added ability to add task

---
+++ tasks/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const randomBytes = require("crypto").randomBytes;
+const PORT = 4000; // task service port
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const tasks = {};
+
+app.post("/tasks", (req, res) => {
+  const task = req.body.data;
+
+  const id = randomBytes(7).toString("hex");
+  tasks[id] = { id: id, data: task };
+  res.status(201).send(tasks[id]);
+});
+
+app.get("/tasks", (req, res) => {
+  res.send(tasks);
+});
+
+app.listen(PORT, () => {
+  console.log("Tasks Service listeing on port 4000");
+});
