d470c05dcadcab66b4a2bdfa5962d40d7dff88f6
dexter <dexterdevmode@gmail.com>
2023-02-27 17:57:29 +0530

    Added event-bus service

---
+++ events/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+
+const PORT = 4005; // Event service running at port 4005
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/events", (req, res) => {
+  const events = req.body;
+  console.log(events);
+  axios.post("http://localhost:4000/events", events).catch((err) => console.log("Tasks service Down"));
+  axios.post("http://localhost:4001/events", events).catch((err) => console.log("Sub-Task service Down"));
+  axios.post("http://localhost:4002/events", events).catch((err) => console.log("Query service Down"));
+
+  res.json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Event service running at port ${PORT}`);
+});
