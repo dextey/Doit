597ea802bdb0358295ef786736cf14df083bc370
dexter <dexterdevmode@gmail.com>
2023-02-27 17:56:46 +0530

    Added query service

---
+++ query/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const PORT = 4002; // Query service running at port 4002
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const tasks = {};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  console.log(type);
+
+  if (type === "TASK_CREATED") {
+    const { id } = data;
+    tasks[id] = data;
+  }
+  if (type === "SUB_TASK_CREATED") {
+    const { tid } = data;
+    tasks[tid]?.subTasks.push(data);
+  }
+
+  console.log(tasks);
+  res.send({});
+});
+
+app.get("/", (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(PORT, () => {
+  console.log(`Query service running at port ${PORT}`);
+});
